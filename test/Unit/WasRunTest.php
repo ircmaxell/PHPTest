b26daed555e602bb9a9d4a1a160a55a3188f5543
Anthony Ferrara <ircmaxell@gmail.com>
2011-11-16 11:51:06 -0500

    A little bit of refactoring

--- test/Unit/WasRunTest.php
+++ test/Unit/WasRunTest.php
@@ -2,19 +2,24 @@
 
 namespace PHPTest\test\Unit;
 
-$test = new WasRun();
+$test = new WasRun('testMethod');
 echo $test->wasRun;
 $test->run();
 echo $test->wasRun;
 
 class WasRun {
     public $wasRun = 0;
+    public $name = '';
+
+    public function __construct($name) {
+        $this->name = $name;
+    }
 
     public function testMethod() {
         $this->wasRun = 1;
     }
 
     public function run() {
-        $this->testMethod();
+        $this->{$this->name}();
     }
 }
